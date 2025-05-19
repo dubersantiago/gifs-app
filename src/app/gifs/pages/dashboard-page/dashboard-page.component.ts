d94992f7f811726ee903bdca6d9fae77bba705fb
Duber Garcia <duber.garcia@solucionessyh>
2025-05-19 16:59:44 -0500

    inicial proyecto

---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SideMenuHeaderComponent } from "../../components/side-menu-header/side-menu-header.component";
+import { SideMenuOptionsComponent } from "../../components/side-menu-options/side-menu-options.component";
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [RouterOutlet, SideMenuHeaderComponent, SideMenuOptionsComponent],
+  templateUrl: './dashboard-page.component.html',
+})
+export default class DashboardPageComponent { }
